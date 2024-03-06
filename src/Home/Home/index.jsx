22f1b750873c5d71e3032e044159a95910bc2797
ersurajkumar1999 <er.surajkumar1999@gmai.com>
2024-03-06 20:42:44 +0530

    home page added

--- src/Home/Home/index.jsx
+++ src/Home/Home/index.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import ImageSlider from '../components/ImageSlider'
+import {
+  Typography, Grid, CardContent, Card
+} from '@mui/material';
+import ServiceSection from '../components/ServiceSection';
+import AboutSection from '../components/AboutSection';
+import TechnologiesSection from '../components/TechnologiesSection';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12} md={12}>
+          <Card sx={{ marginTop: '10px' }}>
+            <CardContent>
+              <ImageSlider />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12}>
+          <Card sx={{ marginTop: '10px' }}>
+            <CardContent>
+              <ServiceSection />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12}>
+          <Card sx={{ marginTop: '10px' }}>
+            <CardContent>
+              <AboutSection />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12}>
+          <Card sx={{ marginTop: '10px' }}>
+            <CardContent>
+              <TechnologiesSection />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
