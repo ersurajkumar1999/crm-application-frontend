ad09b1fcb05520785546fb41fece60d90d04bde5
Er Suraj Kumar <146355358+ersurajkumar1999@users.noreply.github.com>
2024-03-09 21:28:51 +0530

    Update index.jsx

--- src/User/ChatHome/index.jsx
+++ src/User/ChatHome/index.jsx
@@ -7,7 +7,7 @@ import ChatUserInformationSection from './components/ChatUserInformationSection'
 
 
 const ChatHome = () => {
-    const activeUser = true;
+    const activeUser = false;
     return (
         <Container sx={{ marginTop: 1 }} maxWidth="xl">
             <Box py={5} display={`flex`} flexDirection={`column`} alignItems={`center`}>
