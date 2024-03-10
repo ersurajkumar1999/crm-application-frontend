25cadb79c88d4f65a695f1a005beb757927b4ba7
ersurajkumar1999 <er.surajkumar1999@gmai.com>
2024-03-10 23:59:16 +0530

    changes

--- src/User/Feed/index.jsx
+++ src/User/Feed/index.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import {
+    Box, Container, Grid, Paper, Stack,
+    Card, Tabs, useMediaQuery, Tab
+} from "@mui/material";
+import ProfileInformation from './components/ProfileInformation';
 const Feed = () => {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const handleTabChange = (event, newValue) => setSelectedTab(newValue);
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
-        <div>Feed</div>
+        <>
+            <Container sx={{ marginTop: 1 }} maxWidth="xl">
+                <Box py={5} display={`flex`} flexDirection={`column`} alignItems={`center`}>
+                    <Grid container spacing={3} my={2} justifyContent="center">
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Paper sx={{ padding: 1, borderRadius: 1 }} >
+                                <Stack spacing={1}>
+                                    <ProfileInformation />
+                                </Stack>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={6}>
+                            <Paper sx={{ padding: 1, borderRadius: 1 }} >
+                                <Stack spacing={1}>
+                                   sadsda
+                                </Stack>
+                            </Paper>
+                            <Paper sx={{ padding: 1, borderRadius: 1 }} >
+                                <Stack spacing={1}>
+                                    jhgxc
+                                </Stack>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Paper sx={{ padding: 1, borderRadius: 1 }} >
+                                <Stack spacing={1}>
+                                    <ProfileInformation />
+                                </Stack>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </>
     )
 }
-
-export default Feed
+export default Feed;
