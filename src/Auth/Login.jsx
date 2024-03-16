82013461e8c797609b341704115d671b609f435d
ersurajkumar1999 <er.surajkumar1999@gmai.com>
2024-03-16 15:55:24 +0530

    added react-redux

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { useEffect } from 'react'
 import LoginPage from './components/LoginPage'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/dashboard');
+        }
+    })
     return (
         <LoginPage />
     )
